bd153647ca39f5a5c219e083e29f5a1877d87a42
Shoutt4 <jhonvergara437@gmail.com>
2026-03-28 02:07:20 -0400

    tegnologias-habilidades

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 
 use App\Http\Controllers\PortafolioController;
-use App\Http\Controllers\ProyectoController;
-use App\Http\Controllers\HabilidadController;
 
-Route::apiResource('habilidades', HabilidadController::class);
 Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('/portafolios', [PortafolioController::class, 'index']);
@@ -17,15 +14,3 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::delete('/portafolios/{id}', [PortafolioController::class, 'destroy']);
 
 });
-
-
-
-Route::prefix('proyectos')->group(function () {
-    Route::get('/', [ProyectoController::class, 'index']);
-    Route::get('/{id}', [ProyectoController::class, 'show']);
-    Route::post('/', [ProyectoController::class, 'store']);
-    Route::put('/{id}', [ProyectoController::class, 'update']);
-    Route::delete('/{id}', [ProyectoController::class, 'destroy']);
-});
-
-Route::post('/habilidades', [HabilidadController::class, 'store']);
