f3effe17f60f6b394d9b18631c2b4af6b1269c5e
Paredes <jhoel@email.com>
2026-03-29 20:14:29 -0400

    cerrar-sesion

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,23 +5,26 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        ]);
+  ->withRouting(
+    web: __DIR__ . "/../routes/web.php",
+    api: __DIR__ . "/../routes/api.php",
+    commands: __DIR__ . "/../routes/console.php",
+    health: "/up"
+  )
+  ->withMiddleware(function (Middleware $middleware) {
+    $middleware->api(
+      append: [
+        \Illuminate\Cookie\Middleware\EncryptCookies::class,
+        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        \Illuminate\Session\Middleware\StartSession::class,
+      ]
+    );
 
-        $middleware->alias([
-            'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
-        ]);
-
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+    $middleware->alias([
+      "verified" => \App\Http\Middleware\EnsureEmailIsVerified::class,
+    ]);
+  })
+  ->withExceptions(function (Exceptions $exceptions) {
+    //
+  })
+  ->create();
