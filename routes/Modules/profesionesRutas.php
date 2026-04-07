a901a38b05ed3fe281279a3fa9d0be44c9eb3a67
Jhaseft <jhasesaat@gmail.com>
2026-04-07 00:12:17 -0400

    Refactorizacion de Codigo

---
+++ routes/Modules/profesionesRutas.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Usuario\UsuarioController;
+use App\Http\Controllers\Profesion\ProfesionController;
+
+// Rutas para profesiones
+Route::get("/profesiones", [ProfesionController::class, "index"]);
+Route::get("/profesiones/{id}", [ProfesionController::class, "show"]);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    // Info del usuario autenticado
+    Route::get('/usuario', fn(Request $request) => $request->user());
+
+    // Profesiones del usuario
+    Route::get('/usuario/profesiones', [UsuarioController::class, 'getProfesiones']);
+    Route::post('/usuario/profesiones', [UsuarioController::class, 'asignarProfesion']);
+    Route::delete('/usuario/profesiones/{id}', [UsuarioController::class, 'desasignarProfesion']);
+
+    // Editar info del usuario
+    Route::patch('/usuario/informacion', [UsuarioController::class, 'actualizarInformacion']);
+    Route::patch('/usuario/foto', [UsuarioController::class, 'actualizarFoto']);
+});
