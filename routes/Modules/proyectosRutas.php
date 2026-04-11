4d827dd127d22add7a57f28c0907f54df8c938a7
Diaz <owenstokd@gmail.com>
2026-04-11 14:14:04 -0400

    fix autenticacion

--- routes/Modules/proyectosRutas.php
+++ routes/Modules/proyectosRutas.php
@@ -8,10 +8,10 @@ use App\Http\Controllers\Proyecto\ProyectoController;
 // Rutas para elementos relacionados a portafolios
 Route::prefix("/portafolios")->group(function () { // Autenticacion pendiente
     //proyectos
-    Route::get("/proyectos", [ProyectoController::class, "index"]);
-    Route::get("/proyectos/{id}", [ProyectoController::class, "show"])->middleware('auth:sanctum')->name('proyectos.show');
+    Route::get("/proyectos", [ProyectoController::class, "index"])->middleware('auth:sanctum')->name('proyectos.index');
+    //Route::get("/proyectos/{id}", [ProyectoController::class, "show"])->middleware('auth:sanctum')->name('proyectos.show');
     Route::post("/proyectos", [ProyectoController::class, "store"])->middleware('auth:sanctum')->name('proyectos.store');
-    Route::put("/proyectos/{id}", [ProyectoController::class, "update"]);//->middleware('auth:sanctum')->name('proyectos.update');
+    Route::put("/proyectos/{id}", [ProyectoController::class, "update"])->middleware('auth:sanctum')->name('proyectos.update');
     Route::delete("/proyectos/{id}", [ProyectoController::class, "destroy"])->middleware('auth:sanctum')->name('proyectos.destroy');
 
 });
