facfcfa53cfc41f4a1a8dd22a8770101ca7ebc3e
Shoutt4 <jhonvergara437@gmail.com>
2026-04-15 20:30:52 -0400

    flix-config

--- routes/Modules/portafolioRutas.php
+++ routes/Modules/portafolioRutas.php
@@ -2,5 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Portafolio\PublicPortafolioController;
+use App\Http\Controllers\Configuracion\ConfiguracionPortafolioController;
 
+Route::middleware('auth:sanctum')->prefix('configuracion')->group(function () {
+    Route::get('/', [ConfiguracionPortafolioController::class, 'show']);
+    Route::put('/', [ConfiguracionPortafolioController::class, 'update']);
+});
 Route::get('/public/portafolio/{slug}', [PublicPortafolioController::class, 'show']);
