b864e6921643eb4def4b737fee1cc43070bffeaa
Diaz <owenstokd@gmail.com>
2026-04-25 20:46:17 -0400

    fix migrations

--- database/migrations/2026_03_22_231049_create_habilidad_modulo.php
+++ database/migrations/2026_03_22_231049_create_habilidad_modulo.php
@@ -10,29 +10,30 @@ return new class extends Migration {
      
         DB::statement('CREATE EXTENSION IF NOT EXISTS "pgcrypto";');
 
-        /* Schema::create('categoria_habilidad', function (Blueprint $table) {
+        Schema::create('categoria_habilidad', function (Blueprint $table) {
             $table->uuid('id_categoria_habilidad')->primary()->default(DB::raw('gen_random_uuid()'));
             $table->string('nombre',100)->nullable();
-        }); */
+        }); 
 
-        /* Schema::create('nivel_de_habilidad', function (Blueprint $table) {
+            Schema::create('nivel_de_habilidad', function (Blueprint $table) {
             $table->uuid('id_nivel_habilidad')->primary()->default(DB::raw('gen_random_uuid()'));
             $table->string('nivel',50)->nullable();
-        }); */
+        });
 
         Schema::create('habilidad', function (Blueprint $table) {
             $table->uuid('id_habilidad')->primary()->default(DB::raw('gen_random_uuid()'));
             $table->uuid('id_portafolio');
-            //$table->uuid('id_categoria_habilidad');
-            $table->enum('categoria_habilidad', ['blanda','tecnica'])->nullable();
-            $table->enum('nivel', ['Principiante','Intermedio', 'Competente', 'Avanzado', 'Experto'])->nullable();
+            $table->uuid('id_categoria_habilidad');
+            $table->uuid('id_nivel_habilidad');
+            //$table->enum('categoria_habilidad', ['blanda','tecnica'])->nullable();
+            //$table->enum('nivel', ['Principiante','Intermedio', 'Competente', 'Avanzado', 'Experto'])->nullable();
             $table->string('nombre',50)->nullable();
             $table->timestamp('fecha_creacion')->nullable();
             $table->timestamp('fecha_actualizacion')->nullable();
 
             $table->foreign('id_portafolio')->references('id_portafolio')->on('portafolio');
-            //$table->foreign('id_categoria_habilidad')->references('id_categoria_habilidad')->on('categoria_habilidad');
-            //$table->foreign('id_nivel_habilidad')->references('id_nivel_habilidad')->on('nivel_de_habilidad');
+            $table->foreign('id_categoria_habilidad')->references('id_categoria_habilidad')->on('categoria_habilidad');
+            $table->foreign('id_nivel_habilidad')->references('id_nivel_habilidad')->on('nivel_de_habilidad');
         });
     }
 
