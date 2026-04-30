4ad4a36257e828cce5051e84c34214dfc3740005
Diaz <owenstokd@gmail.com>
2026-04-30 15:46:25 -0400

    fix en FA

--- routes/Modules/habilidadesRutas.php
+++ routes/Modules/habilidadesRutas.php
@@ -7,8 +7,10 @@ use App\Http\Controllers\Habilidad\HabilidadController;
 
 Route::prefix('portafolios')->group(function () {
     // Rutas para habilidades
-    Route::get("{idPortafolio?}/habilidades", [HabilidadController::class, "index"])->middleware('auth:sanctum')->name('habilidades.index');   
-    Route::post("{idPortafolio?}/habilidades", [HabilidadController::class, "store"])->middleware('auth:sanctum')->name('habilidades.store');
+    Route::get("{/habilidades", [HabilidadController::class, "index"])->middleware('auth:sanctum')->name('habilidades.index.default');   
+    Route::get("{idPortafolio?}/habilidades", [HabilidadController::class, "index"])->middleware('auth:sanctum')->name('habilidades.index.byIdPortafolio');   
+    Route::post("/habilidades", [HabilidadController::class, "store"])->middleware('auth:sanctum')->name('habilidades.store.default');
+    Route::post("{idPortafolio?}/habilidades", [HabilidadController::class, "store"])->middleware('auth:sanctum')->name('habilidades.store.byIdPortafolio');
     Route::put("/habilidades/{id}", [HabilidadController::class, "update"])->middleware('auth:sanctum')->name('habilidades.update');
     Route::delete("/habilidades/{id}", [HabilidadController::class, "destroy"])->middleware('auth:sanctum')->name('habilidades.destroy');
    
