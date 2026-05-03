acdc69e8602273232132b336a2ed13f25364fe46
Shoutt4 <jhonvergara437@gmail.com>
2026-05-03 06:02:34 -0400

    flix-habilidades

--- routes/Modules/portafolioRutas.php
+++ routes/Modules/portafolioRutas.php
@@ -6,6 +6,18 @@ use App\Http\Controllers\Portafolio\PortafolioPreviewController;
 use App\Http\Controllers\Portafolio\PortafolioLinkController;
 use App\Http\Controllers\Configuracion\ConfiguracionPortafolioController;
 use App\Http\Controllers\Portafolio\ReporteController;
+use App\Http\Controllers\Portafolio\PortafolioEstadisticaController;
+
+
+Route::get(
+    'portafolios/{id_portafolio}/estadisticas',
+    [PortafolioEstadisticaController::class, 'show']
+);
+
+Route::get(
+    'mi-portafolio/estadisticas',
+    [PortafolioEstadisticaController::class, 'show']
+)->middleware('auth:sanctum');
 
 Route::get('/reportes', [ReporteController::class, 'index'])
     ->middleware('auth:sanctum');
