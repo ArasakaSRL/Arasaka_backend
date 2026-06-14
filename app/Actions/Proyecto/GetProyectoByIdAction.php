991fe210dac8604bc235ab811315effa5746c6f8
Diaz <owenstokd@gmail.com>
2026-06-14 18:44:22 -0400

    fix id

--- app/Actions/Proyecto/GetProyectoByIdAction.php
+++ app/Actions/Proyecto/GetProyectoByIdAction.php
@@ -8,7 +8,7 @@ class GetProyectoByIdAction
 {
     public function execute($id)
     {
-        return Proyecto::where('id', $id)
+        return Proyecto::where('id_proyecto', $id)
                     ->where('visible', true)
                     ->with(['tecnologias', 'imagenes'])
                     ->first();
