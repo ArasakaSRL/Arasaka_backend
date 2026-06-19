bff23b78b4f1bf3dde4de877e96c9409e0065021
Shoutt4 <jhonvergara437@gmail.com>
2026-06-19 01:51:00 -0400

    flix-tecnologias

--- app/Services/Tecnologia/TecnologiaService.php
+++ app/Services/Tecnologia/TecnologiaService.php
@@ -12,24 +12,30 @@ class TecnologiaService
             ->paginate($cantidad);
     }
 
-    public function crear(array $data)
-    {
-        $existe = Tecnologia::whereRaw(
-            'LOWER(nombre) = ?',
-            [strtolower(trim($data['nombre']))]
-        )->exists();
-        if ($existe) {
-            throw new HttpResponseException(response()->json([
-                'success' => false,
-                'message' => 'El nombre de la tecnología ya está en uso.',
-                'data' => null
-            ], 422));  
-        }
-        Tecnologia::create($data);
+public function crear(array $data)
+{
+    $existe = Tecnologia::whereRaw(
+        'LOWER(nombre) = ?',
+        [strtolower(trim($data['nombre']))]
+    )->exists();
 
-        return Tecnologia::orderBy('nombre', 'asc')
-            ->paginate(9);
+    if ($existe) {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'El nombre de la tecnología ya está en uso.',
+            'data' => null
+        ], 422));
     }
+
+    if (empty($data['logo'])) {
+        $data['logo'] = 'https://firebasestorage.googleapis.com/v0/b/arasaka-tis.firebasestorage.app/o/tecnologias%2F945529.png?alt=media&token=41dc7bf9-fed8-49f9-9016-dcc068022a19';
+    }
+
+    Tecnologia::create($data);
+
+    return Tecnologia::orderBy('nombre', 'asc')
+        ->paginate(9);
+}
 
     public function mostrar(string $id)
     {
